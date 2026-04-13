2aeeeb0a8d0ef88c95e0ebda5f1b4c814dba3c94
Fábio <otymus27@gmail.com>
2026-04-13 07:57:16 -0300

    Fix favicon and add local server

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8080;
+
+const MIME_TYPES = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.md': 'text/markdown',
+    '.ico': 'image/x-icon',
+};
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    // Remove query strings or hashes
+    filePath = filePath.split('?')[0].split('#')[0];
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = MIME_TYPES[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                // If it's a sub-route in a SPA, we should return index.html
+                // But this is a simple static server. Let's return index.html for unknown paths to support the hash routing better if needed, 
+                // though for hash routing the browser doesn't send the hash to the server.
+                fs.readFile('./index.html', (err, indexContent) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(indexContent, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                res.end();
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log('Press Ctrl+C to stop.');
+});
